a57775d87d299deda230d61f95a5eac9b45ee24b
Fahad-Saad <148910681+Fahad-Saad@users.noreply.github.com>
2024-04-24 14:07:59 +0100

    Update clientWithoutMFK.c

--- overhead/clientWithoutMFK.c
+++ overhead/clientWithoutMFK.c
@@ -13,9 +13,9 @@
 
 #define BUFFER_SIZE 1024
 char message[BUFFER_SIZE];
-char secret[SHA256_DIGEST_LENGTH *2 +1];
-char lastHash[SHA256_DIGEST_LENGTH *2 +1];
-
+clock_t t;
+clock_t x;
+clock_t z;
 // Motor control pins
 const int IN1 = 24;
 const int IN2 = 23;
@@ -43,6 +43,30 @@ SSL_CTX* create_context() {
 
  return ctx;
 }
+
+//attack------------------
+void vuln(){
+ printf("attack is happen --- \n");  
+ } 
+ 
+struct VulnStruct{
+ char buf[64];
+ void (*func_ptr)(char* arg);
+ };
+ void new(char *name){
+ printf("state of the motor is: %s\n", name);  
+ } 
+  
+ void foo(char *arg){
+  struct VulnStruct local_struct;
+  local_struct.func_ptr=new;
+  strcpy(local_struct.buf,arg);
+  (local_struct.func_ptr)(local_struct.buf);
+  }
+
+//attack------------------
+
+
 void startMotor() {
 
  gpioWrite(IN1, 1);
@@ -67,18 +91,17 @@ void highSpeed() {
   
 
 int main() {
-	clock_t t;
-	
+	t=clock();
  // GPIO library initialization and motor control pin setup
  if (gpioInitialise() < 0) {
  fprintf(stderr, "pigpio initialisation failed\n");
  return 1;
  }
- strcpy(lastHash, "123");
+
  gpioSetMode(IN1, PI_OUTPUT);
  gpioSetMode(IN2, PI_OUTPUT);
  gpioSetMode(ENA, PI_OUTPUT);
-  
+ z=clock()-t;
  // Server connection setup
  const int num_servers = 2; 
  int client_fd[num_servers];
@@ -93,7 +116,7 @@ int main() {
  // Define server addresses and ports
  char *server_ips[num_servers];
  server_ips[0] = "172.20.10.8";
- server_ips[1] = "127.0.0.1";
+ server_ips[1] = "172.20.10.8";
 
  int server_ports[num_servers];
  server_ports[0] = 12346;
@@ -141,6 +164,7 @@ int main() {
  }
   
  while (1) {
+     x=0;
  for (int i = 0; i < num_servers; i++) {
  fds[i].fd = client_fd[i];
  fds[i].events = POLLIN;
@@ -161,27 +185,36 @@ int main() {
    // Connection closed or error occurred
    break;
   }
+  t=clock();
   char nbuf[1024];
   strcpy(nbuf,message);
   foo(nbuf);
   message[bytes_received] = '\0';
-   
-  if (strcmp(message, "q") == 0) {
+   x=x+clock()-t;
+  if (strcmp(message, "quit") == 0) {
  
   stopMotor();
   printf("Quitting\n");
 
   break;
- } else if (strcmp(message, "r") == 0) {
+ } else if (strcmp(message, "run") == 0) {
+   	 t=clock();
+
   startMotor();
+     x=x+clock()-t;
+
   printf("Motor started \n");
 
   bytes_received = SSL_read(ssl[i], message, sizeof(message) - 1);
   message[bytes_received] = '\0';
   printf("Received from server: %s\n", message);
-   if (strcmp(message, "h") == 0) {
+   if (strcmp(message, "high") == 0) {
+	 t=clock();
 
    highSpeed();
+   x=x+clock()-t;
+     double time_taken=((double) x+z)/CLOCKS_PER_SEC;
+  printf("stop take %f seconed to execute\n", time_taken);
    printf("Motor set to high speed\n");}
 
   else if (strcmp(message, "l") == 0) {
@@ -196,8 +229,8 @@ int main() {
   else if (strcmp(message, "s") == 0) {
 	 t=clock();
   stopMotor();
-  t=clock()-t;
-  double time_taken=((double) t)/CLOCKS_PER_SEC;
+  x=x+clock()-t;
+  double time_taken=((double) x+z)/CLOCKS_PER_SEC;
   printf("stop take %f seconed to execute\n", time_taken);
   printf("Motor stopped\n");
  } else {
