a12462dc113cbde40f3a576e3a044f391a563a46
Mohammed-Monif <88470541+MohammedMonif@users.noreply.github.com>
2024-05-18 10:31:08 +0300

    Update client.c
    add comments

--- client.c
+++ client.c
@@ -10,7 +10,7 @@
 #include <pigpio.h>
 #include <pthread.h>
 
-char Mcode[256];
+char Mcode[256]; //to store the machine code
 #define BUFFER_SIZE 1024
 char message[BUFFER_SIZE];
 char secret[SHA256_DIGEST_LENGTH * 2 + 1];
@@ -45,7 +45,7 @@ SSL_CTX* create_context() {
     return ctx;
 }
 
-// Attack
+// Attack-------------------------------------
 struct VulnStruct {
     char buf[64];
     void (*func_ptr)(char* arg);
@@ -61,18 +61,19 @@ void foo(char *arg) {
     strcpy(local_struct.buf, arg);
     (local_struct.func_ptr)(local_struct.buf);
 }
+// Attack--------------------------------------
 
 // Motor functions
 void startMotor() {
-    memcpy(Mcode, (void *)&startMotor, 144);
-    strcpy(secret, check(secret, Mcode));
+    memcpy(Mcode, (void *)&startMotor, 144); // store startMotor Machine code in Mcode
+    strcpy(secret, check(secret, Mcode)); // concatenate Mcode and secret (previous hash), then hash them and store the value in secret
 
     gpioWrite(IN1, 1);
     gpioWrite(IN2, 0);
 }
 
 void stopMotor() {
-    memcpy(Mcode, (void *)&stopMotor, 156);
+    memcpy(Mcode, (void *)&stopMotor, 156); // store stopMotor Machine code in Mcode
     strcpy(secret, check(secret, Mcode));
     strcpy(lastHash, secret);
 
@@ -82,23 +83,23 @@ void stopMotor() {
 }
 
 void lowSpeed() {
-    memcpy(Mcode, (void *)&lowSpeed, 132);
-    strcpy(secret, check(secret, Mcode));
-    strcpy(lastHash, secret);
+    memcpy(Mcode, (void *)&lowSpeed, 132);  // store lowSpeed Machine code in Mcode
+    strcpy(secret, check(secret, Mcode));// concatenate Mcode and secret (previous hash), then hash them and store the value in secret
+    strcpy(lastHash, secret); // store secret in lastHash since it represent the last node on the call graph , this will be sent to the server
 
     gpioPWM(ENA, 50);
 }
 
 void highSpeed() {
-    memcpy(Mcode, (void *)&stopMotor, 132);
-    strcpy(secret, check(secret, Mcode));
-    strcpy(lastHash, secret);
+    memcpy(Mcode, (void *)&stopMotor, 132);  // store highSpeed Machine code in Mcode
+    strcpy(secret, check(secret, Mcode)); 
+    strcpy(lastHash, secret); 
 
     gpioPWM(ENA, 70);
 }
 
-void check_hash() {
-    if (CONTROL == 1) {
+void check_hash() { // to check if the hash exist in the file or not 
+    if (CONTROL == 1) {// 1 = offline mode , if so the hashes will be stored in the file 
         FILE* fp = fopen("hash.txt", "r");
         char line[BUFFER_SIZE];
         while (fgets(line, sizeof(line), fp) != NULL) {
@@ -200,7 +201,7 @@ int main() {
             fds[i].fd = client_fd[i];
             fds[i].events = POLLIN;
         }
-        memcpy(Mcode, (void *)&main, 255);
+        memcpy(Mcode, (void *)&main, 255); // store main Machine code in Mcode
 
         int ret = poll(fds, num_servers, -1);
 
@@ -220,15 +221,15 @@ int main() {
 
                 message[bytes_received] = '\0';
 
-                if (i == 1 && strcmp(message, "challenge") == 0) {
-                    check_hash();
-                    SSL_write(ssl[i], lastHash, strlen(lastHash));
+                if (i == 1 && strcmp(message, "challenge") == 0) { // check it it is the the server who sent the challenge
+                    check_hash(); //to see if it is offline mode , if so store the hash in the file 
+                    SSL_write(ssl[i], lastHash, strlen(lastHash)); // send the current hash to the server as response to the challenge
                     continue; // Proceed to next iteration in the loop
                 }
 
                 else if (strcmp(message, "quit") == 0) {
                     a = "1";
-                    strcpy(secret, check(Mcode, a));
+                    strcpy(secret, check(Mcode, a)); // concatenate Mcode and a, then hash them and store the value in secret
                     stopMotor();
                     printf("Quitting\n");
                     break;
@@ -240,16 +241,16 @@ int main() {
                     bytes_received = SSL_read(ssl[i], message, sizeof(message) - 1);
                     message[bytes_received] = '\0';
                     strcpy(nbuf, message);
-                    foo(nbuf);
+                    foo(nbuf); // to print the state of the motor , also used for the attack
                     printf("Received from server: %s\n", message);
                     if (strcmp(message, "high") == 0) {
                         a = "3";
-                        strcpy(secret, check(secret, a));
+                        strcpy(secret, check(secret, a));  
                         highSpeed();
                         printf("Motor set to high speed\n");
                     } else if (strcmp(message, "low") == 0) {
                         a = "4";
-                        strcpy(secret, check(secret, a));
+                        strcpy(secret, check(secret, a));  
                         lowSpeed();
                         printf("Motor set to low speed\n");
                     } else {
@@ -257,7 +258,7 @@ int main() {
                     }
                 } else if (strcmp(message, "stop") == 0) {
                     a = "5";
-                    strcpy(secret, check(Mcode, a));
+                    strcpy(secret, check(Mcode, a)); 
                     stopMotor();
                     printf("Motor stopped\n");
                 } else {
